5d3398b0eb0260a39c49eb6e3752262e1e5a6793
saileshbindu <sailesh.bindu189@gmail.com>
2024-05-10 00:33:35 +0530

    lower case removed

--- src/Xcountriessearch.jsx
+++ src/Xcountriessearch.jsx
@@ -1,41 +1,57 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import './xcountriessearch.css'
+import "./xcountriessearch.css";
 
-const Xcountriessearch = () =>{
-const [countries, setCountries] = useState([])
-const [searchVal, setsearchVal]=useState("")
-const countriesURL = "https://restcountries.com/v3.1/all";
+const Xcountriessearch = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchVal, setsearchVal] = useState("");
+  const countriesURL = "https://restcountries.com/v3.1/all";
 
-useEffect(()=>{
+  useEffect(() => {
     fetch(countriesURL)
-    .then((response) =>response.json())
-    .then((data) => setCountries(data))
-    .catch((error)=> console.log(error))
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-},[]);
+  const filteredCountries = countries.filter((item) =>
+    item.name.common.includes(searchVal)
+  );
 
+  const CountryCard = ({ imageURL, imageAlt, title }) => {
+    return (
+      <div className="courtriesMain">
+        <img src={imageURL} alt={imageAlt} />
+        <h3>{title}</h3>
+      </div>
+    );
+  };
 
-const filteredCountries = countries.filter((item)=>item.name.common.toLowerCase().includes(searchVal.toLowerCase()))
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchVal}
+        placeholder="Search"
+        onChange={(e) => setsearchVal(e.target.value)}
+      />
 
-
-const CountryCard = ({imageURL, imageAlt, title}) =>{
-return(<div className="courtriesMain">
-<img src={imageURL} alt={imageAlt} />
-<h3>{title}</h3>
-</div>)
-}
-
-return(<div>
-    <input type="text" value={searchVal} placeholder="Search" onChange={(e) => setsearchVal(e.target.value)}/>
-   
-       {filteredCountries.length === 0 ? (<div>No result Found</div>) :  ( <div className="countryCard">{filteredCountries.map((item) =>(
-            <CountryCard key={item.cca3} imageURL={item.flags.png} imageAlt={item.flags.alt} title={item.name.common} />
-        ))}</div>)}
-        
-  
-</div>)
-}
-
+      {filteredCountries.length === 0 ? (
+        <div>No result Found</div>
+      ) : (
+        <div className="countryCard">
+          {filteredCountries.map((item) => (
+            <CountryCard
+              key={item.cca3}
+              imageURL={item.flags.png}
+              imageAlt={item.flags.alt}
+              title={item.name.common}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Xcountriessearch;
